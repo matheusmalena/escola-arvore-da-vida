a538531c13423907f3c20f0355c4c36599827fe0
Matheus Malena <matheus@tew.one>
2024-07-02 14:15:16 -0300

    start project

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <Header/>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+:root {
+  --verde-escuro: #0d4c3b;
+  --branco: #fff;
+  --preto: #0f0f0f;
+  --verde-claro: #6c9087;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+    <nav>
+        <div>
+            <img src="../assets/logo-sem-fundo.png" alt="">
+        </div>
+        <div>
+            <ul>
+            <li><a href="#sobre">Sobre Nós</a></li>
+            <li><a href="#servicos">Serviços</a></li>
+            <li><a href="#extracurricular">Atividades</a></li>
+            <li><a href="#estrutura">Estrutura</a></li>
+            <li><a href="#contato">Contato</a></li>
+        </ul>
+        </div>
+        <div>
+            <button>Fale conosco</button>
+        </div>
+    </nav>
+    
+  </template>
+  
+  <script>
+  export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Header',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <style scoped>
+    nav{
+        background-color: var(--verde-escuro);
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+    }
+
+    nav img {
+        width: 150px;
+        height: auto;
+    }
+
+    nav ul{
+        display: flex;
+        flex-direction: row;
+    }
+
+    nav button{
+        background-color: var(--branco);
+        border: 1px solid var(--branco);
+        border-radius: 10px;
+        padding: 0.6rem;
+    }
+
+  </style>
+  

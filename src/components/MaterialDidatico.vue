002d375eadc3381ded830ad2631a2f18824a7c8c
Matheus Malena <matheus@tew.one>
2024-08-09 14:23:22 -0300

    update

--- src/components/MaterialDidatico.vue
+++ src/components/MaterialDidatico.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="material-didatico">
-    <h1 class="title">MATERIAL <span class="highlight">DIDÁTICO</span></h1>
+    <h2 class="">MATERIAL <span class="highlight">DIDÁTICO</span></h2>
     <div class="cards">
       <!-- Card 1 -->
       <div class="card" :class="{'active': activeCard === 0}" @mouseover="setActiveCard(0)" @mouseleave="resetActiveCard">
@@ -10,29 +10,39 @@
           <button class="btn">CONHEÇA &#x2197;</button>
         </div>
       </div>
-      <!-- Card 2 -->
-      <div class="card" :class="{'active': activeCard === 1}" @mouseover="setActiveCard(1)" @mouseleave="resetActiveCard">
-        <img src="path_to_image_2.jpg" alt="" class="card-img">
-        <div class="card-content">
-          <h2>EF ANOS INICIAIS</h2>
-          <button class="btn">CONHEÇA &#x2197;</button>
-        </div>
-      </div>
       <!-- Card 3 -->
       <div class="card" :class="{'active': activeCard === 2}" @mouseover="setActiveCard(2)" @mouseleave="resetActiveCard">
-        <img src="path_to_image_3.jpg" alt="" class="card-img">
+        <img src="" alt="" class="card-img">
         <div class="card-content">
-          <h2>EF ANOS FINAIS</h2>
+          <h2>ENSINO FUNDAMENTAL I</h2>
           <button class="btn">CONHEÇA &#x2197;</button>
         </div>
       </div>
-      <!-- Card 4 -->
-      <div class="card" :class="{'active': activeCard === 3}" @mouseover="setActiveCard(3)" @mouseleave="resetActiveCard">
-        <img src="path_to_image_4.jpg" alt="" class="card-img">
-        <div class="card-content">
-          <h2>ENSINO MÉDIO</h2>
-          <button class="btn">CONHEÇA &#x2197;</button>
-        </div>
+    </div>
+    <div class="features">
+      <!-- Feature 1 -->
+      <div class="feature">
+        <img src="icon_path_1.png" alt="" class="feature-icon">
+        <h3>Habilidades e competências</h3>
+        <p>Fomentamos a interdisciplinaridade em todas as áreas do conhecimento.</p>
+      </div>
+      <!-- Feature 2 -->
+      <div class="feature">
+        <img src="icon_path_2.png" alt="" class="feature-icon">
+        <h3>Método fônico</h3>
+        <p>Utilizamos um método de alfabetização que valoriza a cultura letrada.</p>
+      </div>
+      <!-- Feature 3 -->
+      <div class="feature">
+        <font-awesome-icon :icon="['fas', 'cross']" />
+        <h3>Cosmovisão cristã aplicada</h3>
+        <p>Nossa prática pedagógica é fundamentada na teologia cristã reformada.</p>
+      </div>
+      <!-- Feature 4 -->
+      <div class="feature">
+        <img src="icon_path_4.png" alt="" class="feature-icon">
+        <h3>Aprendizagem significativa</h3>
+        <p>Organizamos sequências didáticas para o aluno construir o entendimento dos objetos de conhecimento de cada etapa.</p>
       </div>
     </div>
   </div>
@@ -60,27 +70,21 @@ export default {
 .material-didatico {
   text-align: center;
   padding: 20px;
-}
-
-.title {
-  font-size: 48px;
-  margin-bottom: 20px;
-}
-
-.highlight {
-  color: red;
+  margin: 50px 0 30px 0;
 }
 
 .cards {
   display: flex;
-  justify-content: space-around;
-  margin-bottom: 40px;
+  justify-content: center;
+  margin: 80px 0 40px 0;
   transition: all 0.3s ease;
+  gap: 1rem;
 }
 
 .card {
   position: relative;
-  width: 20%;
+  width: 300px;
+  height: auto;
   overflow: hidden;
   border-radius: 10px;
   transition: all 0.3s ease;
@@ -88,12 +92,12 @@ export default {
 }
 
 .card.active {
-  width: 35%;
+  width: 30%;
   transform: scale(1.1);
 }
 
 .card-img {
-  width: 100%;
+  max-width: 500px;
   height: auto;
   transition: all 0.3s ease;
 }
@@ -118,5 +122,32 @@ export default {
 
 .btn:hover {
   background-color: #333;
+}
+
+
+.features {
+  display: flex;
+  justify-content: space-around;
+  margin-top: 20px;
+}
+
+.feature {
+  text-align: center;
+  width: 20%;
+  border-right: 1px solid black;
+}
+
+.feature-icon {
+  margin-bottom: 10px;
+}
+
+.feature h3 {
+  margin-bottom: 10px;
+  font-size: 18px;
+}
+
+.feature p {
+  font-size: 14px;
+  color: gray;
 }
 </style>
